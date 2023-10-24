2f8a696b9664a03381a3bfbe47b32c7c05d068c7
OlegSenko <senkooleg@gmail.com>
2023-10-24 23:35:38 +0300

    add express

---
+++ server/controller/user.controller.js
@@ -0,0 +1,12 @@
+class UserController {
+  async createUser(req, res) {
+    const { name, surname } = req.body;
+    console.log(name, surname);
+    res.json("ok");
+  }
+  async getUsers(req, res) {}
+  async getUser(req, res) {}
+  async updateUser(req, res) {}
+  async deleteUser(req, res) {}
+}
+module.exports = new UserController();
