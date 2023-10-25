cdb4ccd2b0d83a3be60a260f0fb0140e05ae9be5
OlegSenko <senkooleg@gmail.com>
2023-10-25 10:31:12 +0300

    add static index

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const userRouter = require("./routes/user.routes");
 
@@ -9,6 +10,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+});
 app.use("/api", userRouter);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
